a36759db6bcadb7157dea0a768547e2d660915c7
sharmask <sharmask@ucalgary.ca>
2016-10-24 15:29:46 -0600

    First commit

---
+++ app/src/main/java/com/example/sharmask/myapplication/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.sharmask.myapplication;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "Testing: ";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Log.d(TAG, "My debug message");
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+                                   @Override
+                                   public void onClick(View view) {
+                                       Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                                               .setAction("Action", null).show();
+                                       TextView myText = (TextView) findViewById(R.id.myText);
+                                       myText.setText("SOMETHING");
+                                   }
+                               }
+        );
+
+        Button actionButton = (Button) findViewById(R.id.button);
+        actionButton.setOnClickListener(new View.OnClickListener() {
+            private int buttonState = 1;
+            @Override
+            public void onClick(View actionButton) {
+                if (buttonState % 2 == 0) {
+                    actionButton.setBackgroundColor(Color.RED);
+                } else {
+                    actionButton.setBackgroundColor(Color.BLUE);
+                }
+                buttonState++;
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
